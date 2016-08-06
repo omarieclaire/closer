e56a70317a21c496e3066ce076bae0ce27a0318a
Marie Claire LeBlanc Flanagan <marieleblancflanagan@maries-macbook-pro.local>
2016-08-06 20:03:13 +0200

    Fix imports in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-// here is where I set the size of the screen
-
-
+#include "ofMain.h"
 #include "ofApp.h"
+
+// here is where I set the size of the screen
 
 int main() {
     ofSetupOpenGL(1280, 720, OF_GAME_MODE);
